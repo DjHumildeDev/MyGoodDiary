a54c0f4517586467a6fac9c5a18185e5323f1f58
DjHumildeDev <choches61@gmail.com>
2026-03-18 02:05:44 +0100

    primer commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyGoodDiary;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ obj/Debug/net10.0-android/android/src/crc644217c15f56d4d026/MainActivity.java
@@ -0,0 +1,46 @@
+package crc644217c15f56d4d026;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MyGoodDiary.MainActivity, MyGoodDiary", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MyGoodDiary.MainActivity, MyGoodDiary", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MyGoodDiary.MainActivity, MyGoodDiary", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
