c108b2b07924594949b1a0bfc0bdea5342f55113
DjHumildeDev <choches61@gmail.com>
2026-04-02 16:32:44 +0200

    ya me esta vacilando el puto git

--- obj/Debug/net10.0-maccatalyst/maccatalyst-x64/MyGoodDiary.AssemblyInfo.cs
+++ obj/Debug/net10.0-maccatalyst/maccatalyst-x64/MyGoodDiary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGoodDiary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9dc06a14d7efb19e63eb6e1b2e9fdcde1381189e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+919456df28dc06f866ec24a36ec339437b6cbc31")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGoodDiary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGoodDiary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
